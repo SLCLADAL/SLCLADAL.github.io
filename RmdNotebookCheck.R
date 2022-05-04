e887d82f7904457c49dfff38ef030e63d1e402c7
Martin Schweinberger <martin.schweinberger.hh@gmail.com>
2022-05-04 19:57:31 +1000

    after update page updates

--- RmdNotebookCheck.R
+++ RmdNotebookCheck.R
@@ -1,7 +1,7 @@
 # check if notebooks load superfluous packages
-knitr::purl("basicstatz.Rmd", documentation = 0)
+knitr::purl("corplingr.Rmd", documentation = 0)
 
 #install.packages('devtools')
 library(devtools)
 #install_github("MichaelChirico/funchir")
-funchir::stale_package_check('basicstatz.R')
+funchir::stale_package_check('corplingr.R')
