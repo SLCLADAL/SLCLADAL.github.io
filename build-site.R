98f046b4ef45baa8aaec4695e156edd31916c4c5
Sam Hames <sam@hames.id.au>
2022-08-11 11:11:41 +1000

    Make sure all necessary content is in the content folder

--- build-site.R
+++ build-site.R
@@ -12,6 +12,12 @@ exclude <- list('etpro.Rmd', 'eyews.Rmd', 'git.Rmd', 'mcol.Rmd', 'mixreg.Rmd', '
 to_process <- setdiff(list.files(path="content", pattern = "Rmd"), exclude)
 
 # Note that we can't just use the render_site functionality, as that doesn't work with bookdowns html_document2 format.
-for (file in to_process) {
-	rmarkdown::render(file, output_dir = "built_site")
+for (source_file in to_process) {
+	rmarkdown::render(
+		file.path(
+			"content", source_file),
+			output_dir = "built_site",
+			envir = new.env(),
+			clean = TRUE
+		)
 }
