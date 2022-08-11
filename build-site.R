98df51cd0469c3a7618b969a4302d45f19d07b09
Sam Hames <sam@hames.id.au>
2022-08-11 10:45:32 +1000

    Move .Rmd source files to a folder off the toplevel

--- build-site.R
+++ build-site.R
@@ -9,9 +9,9 @@ exclude <- list('etpro.Rmd', 'eyews.Rmd', 'git.Rmd', 'mcol.Rmd', 'mixreg.Rmd', '
 
 # Note we're randomly ordering the list - if the build fails because of this, we have a problem
 # in the file that fails.
-to_process <- setdiff(list.files(pattern = "Rmd"), exclude)
+to_process <- setdiff(list.files(path="content", pattern = "Rmd"), exclude)
 
 # Note that we can't just use the render_site functionality, as that doesn't work with bookdowns html_document2 format.
 for (file in to_process) {
-	rmarkdown::render(file, output_dir = "output")
+	rmarkdown::render(file, output_dir = "built_site")
 }
