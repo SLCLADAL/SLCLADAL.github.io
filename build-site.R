1361cbc5774ba0787d33485b2db6b32637fa0b5a
Sam Hames <sam@hames.id.au>
2022-08-11 11:40:15 +1000

    Update the build-site script to continue on after errors, then report them at the end with a failing exit code

--- build-site.R
+++ build-site.R
@@ -11,13 +11,25 @@ exclude <- list('etpro.Rmd', 'eyews.Rmd', 'git.Rmd', 'mcol.Rmd', 'mixreg.Rmd', '
 # in the file that fails.
 to_process <- setdiff(list.files(path="content", pattern = "Rmd"), exclude)
 
+failed_files <- list()
 # Note that we can't just use the render_site functionality, as that doesn't work with bookdowns html_document2 format.
 for (source_file in to_process) {
-	rmarkdown::render(
-		file.path(
-			"content", source_file),
-			output_dir = "built_site",
-			envir = new.env(),
-			clean = TRUE
-		)
+	tryCatch(
+		{
+			rmarkdown::render(
+				file.path("content", source_file),
+				output_dir = "built_site",
+				envir = new.env()
+			)
+		},
+		error = function(e){
+			failed_files <- append(failed_files, source_file)
+		}
+	)
 }
+
+print("Failed Files")
+print(failed_files)
+
+# It is an error to return with any failed files
+quit(length(failed_files))
