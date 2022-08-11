3f4414c47df6a6ba630cef4475e33171fc2e99b7
Sam Hames <sam@hames.id.au>
2022-08-11 10:31:26 +1000

    Write output site to a separate directory to simplify folder structure

---
+++ build-site.R
@@ -0,0 +1,17 @@
+library(rmarkdown)
+
+# Manual list of files to exclude from automatic rebuilding
+# Based on analysis, these are either work in progress, event specific, or
+# just unfinished - I think these are all candidates for deletion from the
+#  repository to avoid confusion.
+
+exclude <- list('etpro.Rmd', 'eyews.Rmd', 'git.Rmd', 'mcol.Rmd', 'mixreg.Rmd', 'mmws.Rmd')
+
+# Note we're randomly ordering the list - if the build fails because of this, we have a problem
+# in the file that fails.
+to_process <- setdiff(list.files(pattern = "Rmd"), exclude)
+
+# Note that we can't just use the render_site functionality, as that doesn't work with bookdowns html_document2 format.
+for (file in to_process) {
+	rmarkdown::render(file, output_dir = "output")
+}
