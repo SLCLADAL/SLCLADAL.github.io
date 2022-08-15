023f7e38d1bed7778e5e56de8d232fa814052314
Sam Hames <sam@hames.id.au>
2022-08-15 11:11:36 +1000

    Temporarily remove the webcrawling tutorial until we migrate phantomjs to something else

--- build-site.R
+++ build-site.R
@@ -1,3 +1,5 @@
+# This script builds the whole site - optionally you can pass specific files
+# to build by name and it build just those files.
 library(rmarkdown)
 
 # Manual list of files to exclude from automatic rebuilding
@@ -5,13 +7,20 @@ library(rmarkdown)
 # just unfinished - I think these are all candidates for deletion from the
 #  repository to avoid confusion.
 
-exclude <- list('etpro.Rmd', 'eyews.Rmd', 'git.Rmd', 'mcol.Rmd', 'mixreg.Rmd', 'mmws.Rmd')
+exclude <- list('etpro.Rmd', 'eyews.Rmd', 'git.Rmd', 'mcol.Rmd', 'mixreg.Rmd', 'mmws.Rmd', 'webcrawling.Rmd', 'todo.Rmd')
 
-# Note we're randomly ordering the list - if the build fails because of this, we have a problem
-# in the file that fails.
-to_process <- setdiff(list.files(path="content", pattern = "Rmd"), exclude)
+# Check if we have any command line arguments, otherwise just run on everything.
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args) > 0) {
+	to_process <- args
+} else {
+	to_process <- setdiff(list.files(path="content", pattern = "Rmd"), exclude)
+}
 
 failed_files <- list()
+failure_reasons <- list()
+
 # Note that we can't just use the render_site functionality, as that doesn't work with bookdowns html_document2 format.
 for (source_file in to_process) {
 	tryCatch(
@@ -22,14 +31,21 @@ for (source_file in to_process) {
 				envir = new.env()
 			)
 		},
-		error = function(e){
-			failed_files <- append(failed_files, source_file)
+		error = function(cond){
+			failed_files <<- append(failed_files, source_file)
+			failure_reasons <<- append(failure_reasons, cond)
+		},
+		warning = function(cond){
 		}
 	)
 }
 
-print("Failed Files")
-print(failed_files)
+for(i in seq_along(failed_files)){
+    message(failed_files[i])
+    message(failure_reasons[i])
+    message("")
+}
+
 
 # It is an error to return with any failed files
-quit(length(failed_files))
+quit(save="no", status=length(failed_files))
