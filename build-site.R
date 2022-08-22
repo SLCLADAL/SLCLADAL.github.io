ff7008f5e9d46397e4640c2f28b4963e4bcda536
Sam Hames <sam@hames.id.au>
2022-08-22 11:47:01 +1000

    Quality of life fixes and cleanup (#18)
    * Add missing remotes package install to base.Rmd
    
    * Document necessary packages for basicquant.Rmd
    
    * Add missing packages to installation for basicstatz.Rmd
    
    * Fix typo in citation for clust.Rmd
    
    * Add missing packages install for collocations
    
    * Load hslld.rds directly from the URL for corplingr.Rmd
    
    * Fix typo
    
    * Install missing libraries for dstats
    
    * Add missing package installation for data viz
    
    * Install missing packages for fixreg.Rmd
    
    * Fixes for gviz.Rmd
    
    - Add missing libraries, including the rgeos package to work
      around the restrictive licensing of gpclib
    - Download all of the files necessary to load custom shape files -
      the .shp file by itself is not sufficient
    
    * Add missing package for lex.Rmd
    
    * Update lock file for project dependencies
    
    * Add a cleaner script to rebuild the site, with clearly documented exceptions
    
    * Horrific hack to make the navbar reproducible with the styling
    
    * Remove mixreg.Rmd from the build, add some fixes to the same for the future
    
    * Write output site to a separate directory to simplify folder structure
    
    * Remove toplevel site outputs
    
    * Move .Rmd source files to a folder off the toplevel
    
    * Make sure all necessary content is in the content folder
    
    * Gitignore html files in the content directory - only content in built_site will be served
    
    * Update the build-site script to continue on after errors, then report them at the end with a failing exit code
    
    * Temporarily remove the webcrawling tutorial until we migrate phantomjs to something else
    
    * Avoid side effects on repo when demonstrating load/save
    
    * Cleanup temporary path handling + use right make_image function
    
    * Add additional exclusion file
    
    * Add GPARotation package for surveys.Rmd
    
    * Exclude speech2text.Rmd from build
    
    * Exclude sldata.Rmd
    
    * Exclude more temporary files
    
    * Remove unneeded package install
    
    * Don't use environments in rmarkdown::render
    
    This runs into weird issues when working with the lm/glm package
    possibly due to s4 related something.
    
    Instead run the render function in a totally new session using
    rscript_call functionality.
    
    * Use correct argument names for jaysire call
    
    * Remove bad practice file handling tutorial
    
    * Update to read data from the correct location
    
    * Use right citation key for Breiman RF paper, pull demo package installation into separate block
    
    * Remove dangling tutorial reference
    
    * Update bibliography links to the new content/ folder
    
    * Update links to Rmd files to be links under the content folder
    
    * Update renv.lock
    
    * Link data, content and rscripts folders into the output site
    
    * Handle numerical pain in regression model
    
    * Add fresh build output across the whole site
    
    * Use updated navbar
    
    * Rebuild with correct navbar
    
    * Prepare to check lazy hacks with github pages to avoid migrating to github actions

---
+++ build-site.R
@@ -0,0 +1,63 @@
+# This script builds the whole site - optionally you can pass specific files
+# to build by name and it build just those files.
+library(rmarkdown)
+
+# Manual list of files to exclude from automatic rebuilding
+# Based on analysis, these are either work in progress, event specific, or
+# just unfinished - I think these are all candidates for deletion from the
+#  repository to avoid confusion.
+
+exclude <- list(
+	'etpro.Rmd',
+	'eyews.Rmd',
+	'git.Rmd',
+	'mcol.Rmd',
+	'mixreg.Rmd',
+	'mmws.Rmd',
+	'webcrawling.Rmd',
+	'todo.Rmd',
+	'txt2rda.Rmd',
+	'speech2text.Rmd',
+	'sldata.Rmd',
+	'rmd2html.Rmd',
+	'rmd2jupyter.Rmd',
+	'simulatedata.Rmd'
+)
+
+# Check if we have any command line arguments, otherwise just run on everything.
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args) > 0) {
+	to_process <- args
+} else {
+	to_process <- setdiff(list.files(path="content", pattern = "Rmd"), exclude)
+}
+
+failed_files <- list()
+failure_reasons <- list()
+
+# Note that we can't just use the render_site functionality, as that doesn't work with bookdowns html_document2 format.
+for (source_file in to_process) {
+	tryCatch(
+		xfun::Rscript_call(
+		  rmarkdown::render,
+		  args = list(file.path("content", source_file), output_dir = "built_site"),
+		),
+		error = function(cond){
+			failed_files <<- append(failed_files, source_file)
+			failure_reasons <<- append(failure_reasons, cond)
+		},
+		warning = function(cond){
+		}
+	)
+}
+
+for(i in seq_along(failed_files)){
+    message(failed_files[i])
+    message(failure_reasons[i])
+    message("")
+}
+
+
+# It is an error to return with any failed files
+quit(save="no", status=length(failed_files))
