92bdf83ac780a96a980abf0987681be95a943415
MartinSchweinberger <martin.schweinberger.hh@gmail.com>
2024-03-04 21:31:15 +1000

    kwicwords update

---
+++ rscripts/kwicwords.R
@@ -0,0 +1,28 @@
+require(dplyr)
+require(tidyr)
+require(quanteda)
+
+kwic_words <- function(x){
+  x %>%
+  # Convert kwic_keyword to a data frame
+  as.data.frame() %>%
+  # select columns 'pre', 'keyword', and 'post'
+  dplyr::select(pre, post) %>%
+  # reshape the data frame from wide to long format using gather
+  tidyr::gather(type, words, pre:post) %>%
+  # extract the 'words' column and collapse into a single character string
+  dplyr::pull(words) %>%
+  paste0(collapse = " ") %>%
+  # tokenize the text
+  quanteda::tokens() %>%
+  # unlist the tokens to create a data frame
+  unlist() %>%
+  as.data.frame() %>%
+  # rename the column to 'token'
+  dplyr::rename(token = 1) %>%
+  # group by 'token' and count the occurrences
+  dplyr::group_by(token) %>%
+  dplyr::summarise(n = n()) %>%
+  # add column stating where the frequency list is 'from'
+  dplyr::mutate(type = "kwic")
+}
