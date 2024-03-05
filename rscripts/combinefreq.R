3bb0dc767da802a166f7c6e7e0611ac4cdc2c01d
MartinSchweinberger <martin.schweinberger.hh@gmail.com>
2024-03-05 14:25:49 +1000

    coll cb update

--- rscripts/combinefreq.R
+++ rscripts/combinefreq.R
@@ -8,8 +8,5 @@ combinefreq <- function(x, y){
                 kwic = n.y) %>%
   dplyr::select(-type.x, -type.y) %>%
   # replace NA values with 0 in 'corpus' and 'kwic' columns
-  tidyr::replace_na(list(corpus = 0, kwic = 0)) %>%
-  # calculate the difference between 'corpus' and 'kwic' and create a new column 'corpus'
-  dplyr::mutate(corpuswokwic = corpus - kwic,
-                corpuswokwic = ifelse(corpuswokwic < 0, 0, corpuswokwic))
+  tidyr::replace_na(list(corpus = 0, kwic = 0))
 }
