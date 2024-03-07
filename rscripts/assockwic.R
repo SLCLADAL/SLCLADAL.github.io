ea6ce135914b3ab0e74c3e3b2c2c900726d67db9
MartinSchweinberger <martin.schweinberger.hh@gmail.com>
2024-03-07 12:44:06 +1000

    coll update

--- rscripts/assockwic.R
+++ rscripts/assockwic.R
@@ -1,38 +1,55 @@
 require(dplyr)
-source("https://slcladal.github.io/rscripts/loglik.R")
+# x must have columns with 
+# token: word 
+# corpus: frequency of token in corpus
+# kwic: frequency of token in kwic
 assockwic <- function(x){
   x %>%
     # work row-wise
     dplyr::rowwise() %>%
-    # add N
-    dplyr::mutate(N = sum(O11, O12, O21, O22)) %>%
+    dplyr::filter(corpus > 0) %>%
+    dplyr::mutate(corpus = as.numeric(corpus),
+                  kwic = as.numeric(kwic)) %>%
+    dplyr::mutate(corpus= corpus-kwic,
+                  C1 = sum(kwic),
+                  C2 = sum(corpus),
+                  N = C1 + C2) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(R1 = corpus+kwic,
+                  R2 = N - R1,
+                  O11 = kwic,
+                  O12 = R1-O11,
+                  O21 = C1-O11,
+                  O22 = C2-O12) %>%
+    dplyr::mutate(E11 = (R1 * C1) / N,
+                  E12 = (R1 * C2) / N,
+                  E21 = (R2 * C1) / N,
+                  E22 = (R2 * C2) / N) %>%
+    dplyr::select(-corpus, -kwic) %>%
     # calculate fishers' exact test
     dplyr::mutate(p = as.vector(unlist(fisher.test(matrix(c(O11, O12, O21, O22), 
                                                           ncol = 2, byrow = T))[1]))) %>%
     
-    # extract x2 statistics
-    dplyr::mutate(x2 = as.vector(unlist(chisq.test(matrix(c(O11, O12, O21, O22),   
-                                                          ncol = 2, byrow = T),  simulate.p.value = TRUE)[1]))) %>%
-    # extract expected frequency
     
-    dplyr::mutate(expected = as.vector(unlist(chisq.test(matrix(c(O11, O12, O21, O22), 
-                                                                ncol = 2, byrow = T),  simulate.p.value = TRUE)$expected[1]))) %>%
+    # extract x2 statistics
+    dplyr::mutate(X2 = (O11-E11)^2/E11 + (O12-E12)^2/E12 + (O21-E21)^2/E21 + (O22-E22)^2/E22) %>%
     
     # extract association measures
-    dplyr::mutate(phi = sqrt((x2 / N)),
-                  dice = (2 * O11) / (2 * (O11 + O12 + O21)),
-                  MI = (O11 / N) * 
-                    log2((O11 / N) / ((O11+O12) / N) * 
-                           ((O11+O21) / N) ),
+    dplyr::mutate(phi = sqrt((X2 / N)),
+                  Dice = (2 * O11) / (R1 + C1),
+                  MI = log2(O11 / E11),
+                  MS = min((O11/C1), (O11/R1)),
+                  t.score = (O11 - E11) / sqrt(O11),
+                  z.score = (O11 - E11) / sqrt(E11),
                   PMI = log2( (O11 / N) / ((O11+O12) / N) * 
                                 ((O11+O21) / N) ),
-                  t = (O11 - expected) / sqrt(expected)) %>%
+                  DeltaP12 = (O11 / (O11 + O12)) - (O21 / (O21 + O22)),
+                  DeltaP21 =  (O11 / (O11 + O21)) - (O21 / (O12 + O22)),
+                  DP = (O11 / R1) - (O21 / R2),
+                  OddsRatio = log(((O11 + 0.5) * (O22 + 0.5))  / ( (O12 + 0.5) * (O21 + 0.5) )),
+                  # calculate LL aka G2
+                  G2 = 2 * (O11 * log(O11 / E11) + O12 * log(O12 / E12) + O21 * log(O21 / E21) + O22 * log(O22 / E22))) %>%
     
-    # calculate odds ration
-    dplyr::mutate(OddsRatio = ifelse(min(c(O11, O12, O21, O22) > 0),  ((O11/O12) / (O21/O22)), 0)) %>%
-    
-    # calculate loglikratio
-    dplyr::mutate(LogLik = calculate_log_likelihood(O11, O12, O21, O22)) %>%
     
     # simplify significance
     dplyr::mutate(Significance = dplyr::case_when(p <= .001 ~ "p<.001",
--- rscripts/assocstats.R
+++ rscripts/assocstats.R
@@ -1,5 +1,11 @@
 require(dplyr)
-source("https://slcladal.github.io/rscripts/loglik.R")
+# x must have columns with 
+# w1: word 1
+# w2: word 2
+# O11: co-occurrence frequency of w1 and w2
+# O12: co-occurrence frequency of w1 without w2
+# O21: frequency of w2 without w1
+# O22: frequency of words in corpus without w1 and w2
 assocstats <- function(x, term, coocfreq, termcoocfreq){
   x %>%
     # determine Term
@@ -11,35 +17,38 @@ assocstats <- function(x, term, coocfreq, termcoocfreq){
     # work row-wise
     dplyr::rowwise() %>%
     # add N
-    dplyr::mutate(N = sum(O11, O12, O21, O22)) %>%
+    dplyr::mutate(N = sum(O11, O12, O21, O22),
+                  R1 = O11 + O12,
+                  R2 = O21 + O22,
+                  C1 = O11 + O21,
+                  C2 = O12 + O22) %>%
+    dplyr::mutate(E11 = R1 * C1 / N, 
+                  E12 = R1 * C2 / N,
+                  E21 = R2 * C1 / N, 
+                  E22 = R2 * C2 / N) %>%
     # calculate fishers' exact test
     dplyr::mutate(p = as.vector(unlist(fisher.test(matrix(c(O11, O12, O21, O22), 
                                                         ncol = 2, byrow = T))[1]))) %>%
 
     # extract x2 statistics
-    dplyr::mutate(x2 = as.vector(unlist(chisq.test(matrix(c(O11, O12, O21, O22),   
-                                                        ncol = 2, byrow = T),  simulate.p.value = TRUE)[1]))) %>%
-    # extract expected frequency
-
-    dplyr::mutate(expected = as.vector(unlist(chisq.test(matrix(c(O11, O12, O21, O22), 
-                                                              ncol = 2, byrow = T),  simulate.p.value = TRUE)$expected[1]))) %>%
-
+    dplyr::mutate(X2 = (O11-E11)^2/E11 + (O12-E12)^2/E12 + (O21-E21)^2/E21 + (O22-E22)^2/E22) %>%
+    
     # extract association measures
-    dplyr::mutate(phi = sqrt((x2 / N)),
-                dice = (2 * O11) / (2 * (O11 + O12 + O21)),
-                MI = (O11 / N) * 
-                  log2((O11 / N) / ((O11+O12) / N) * 
-                         ((O11+O21) / N) ),
-                PMI = log2( (O11 / N) / ((O11+O12) / N) * 
-                              ((O11+O21) / N) ),
-                t = (O11 - expected) / sqrt(expected)) %>%
-
-    # calculate odds ration
-    dplyr::mutate(OddsRatio = ifelse(min(c(O11, O12, O21, O22) > 0),  ((O11/O12) / (O21/O22)), 0)) %>%
-
-    # calculate loglikratio
-    dplyr::mutate(LogLik = calculate_log_likelihood(O11, O12, O21, O22)) %>%
-
+    dplyr::mutate(phi = sqrt((X2 / N)),
+                  Dice = (2 * O11) / (R1 + C1),
+                  MI = log2(O11 / E11),
+                  MS = min((O11/C1), (O11/R1)),
+                  t.score = (O11 - E11) / sqrt(O11),
+                  z.score = (O11 - E11) / sqrt(E11),
+                  PMI = log2( (O11 / N) / ((O11+O12) / N) * 
+                                ((O11+O21) / N) ),
+                  DeltaP12 = (O11 / (O11 + O12)) - (O21 / (O21 + O22)),
+                  DeltaP21 =  (O11 / (O11 + O21)) - (O21 / (O12 + O22)),
+                  DP = (O11 / R1) - (O21 / R2),
+                  OddsRatio = log(((O11 + 0.5) * (O22 + 0.5))  / ( (O12 + 0.5) * (O21 + 0.5) )),
+                  # calculate LL aka G2
+                  G2 = 2 * (O11 * log(O11 / E11) + O12 * log(O12 / E12) + O21 * log(O21 / E21) + O22 * log(O22 / E22))) %>%
+    
     # simplify significance
     dplyr::mutate(Significance = dplyr::case_when(p <= .001 ~ "p<.001",
                                                 p <= .01 ~ "p<.01",
