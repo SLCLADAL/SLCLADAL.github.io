95bfea5689c0150032670d3c6351e85d1be7064d
MartinSchweinberger <martin.schweinberger.hh@gmail.com>
2024-03-14 10:57:41 +1000

    extracttext function

---
+++ rscripts/extracttext.R
@@ -0,0 +1,49 @@
+# extract names vector of texts from data frame
+# the data frame must have at least 
+# a column entitled file and 
+# a column entitled clean_text
+
+
+
+# required packages
+require(dplyr)
+
+
+extracttext <- function(x){
+  x %>%
+    
+    # group by file
+    dplyr::group_by(file) %>%
+    
+    # concatenate the cleaned text into a single string for each group
+    dplyr::summarise(text = paste0(text_clean, collapse = " ")) %>%
+    
+    # remove grouping
+    dplyr::ungroup() %>%
+    
+    # extract the 'text' column
+    dplyr::pull(text) %>%
+    
+    # remove extra spaces in the resulting character vector
+    
+    stringr::str_squish() -> ctext  # Assign the cleaned and formatted text to 'ctexts'
+  
+  # extract names
+  x %>%
+    
+    # froup by file
+    dplyr::group_by(file) %>%
+    
+    # concatenate the cleaned text into a single string for each group
+    dplyr::summarise(text = paste0(text_clean, collapse = " ")) %>%
+    
+    # remove grouping
+    dplyr::ungroup() %>%
+    
+    # extract the 'text' column
+    dplyr::pull(file) -> names(ctext)
+  
+  # return result
+  return(ctext)
+
+}
