248b50a19f07b1373751b74ed3957002a45b7da1
MartinSchweinberger <martin.schweinberger.hh@gmail.com>
2024-03-19 16:01:59 +1000

    update loadtxt

--- rscripts/loadtxts.R
+++ rscripts/loadtxts.R
@@ -8,6 +8,9 @@ loadtxts <- function(x){
                     
                     # full paths - not just the names of the files
                     full.names = T) 
+  #remove test.txt from file list
+  fls <- fls[!stringr::str_detect(".*test.txt")]
+  
   # loop over the vector 'myfiles' that contains paths to the data
   txts <- sapply(fls, function(x){
     
