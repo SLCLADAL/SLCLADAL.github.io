e01e8961f88eb2d977077f06bd5fb95c88d707db
MartinSchweinberger <martin.schweinberger.hh@gmail.com>
2024-03-22 14:27:03 +1000

    coll and ta updates

--- rscripts/savetxts.R
+++ rscripts/savetxts.R
@@ -9,14 +9,6 @@ savetxts <- function(x){
     # unlist the i-th element in x to get the text content
     text_content <- unlist(x[i])
     
-    # add names
-    #names(text_content) <- ifelse(is.null(names(text_content)) == TRUE, 
-    #                              paste0("text", 
-    #                                     stringr::str_pad(1:length(text_content), 
-    #                                                      width = nchar(max(length(text_content))), 
-    #                                                      pad = "0")),
-    #                              names(text_content))
-    
     # construct the file path using the 'here' package
     file_path <- paste0(here::here("notebooks/MyOutput/"), names(x)[i])
     
