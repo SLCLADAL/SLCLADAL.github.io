963f2bb679cde134d25f83571f3ea62ee1f08f96
MartinSchweinberger <martin.schweinberger.hh@gmail.com>
2024-03-27 12:21:19 +1000

    amtool update

--- rscripts/prepam.R
+++ rscripts/prepam.R
@@ -3,9 +3,45 @@
 
 # load required packages
 require(dplyr)
+require(stringr)
+require(tokenizers)
+require(quanteda)
+require(tidytext)
+
 
 prepam <- function(x){
-  x %>%  dplyr::mutate(N = sum(O11)) %>%
+  x %>% 
+    # concatenate the elements in the 'text' object
+    paste0(collapse = " ") %>%
+    # separate possessives and contractions
+    stringr::str_replace_all(fixed("'"), fixed(" '")) %>%
+    stringr::str_replace_all(fixed("’"), fixed(" '")) %>%
+    # split text into sentences
+    tokenizers::tokenize_sentences() %>%
+    # unlist sentences
+    unlist() %>%
+    # remove non-word characters
+    stringr::str_replace_all("\\W", " ") %>%
+    stringr::str_replace_all("[^[:alnum:] ]", " ") %>%
+    # remove superfluous white spaces
+    stringr::str_squish() %>%
+    # convert to lower case and save in 'sentences' object
+    tolower() %>%
+  # tokenize the 'sentences' data using quanteda package
+    quanteda::tokens() %>%
+    # create a document-feature matrix (dfm) using quanteda
+    quanteda::dfm() %>%
+    # create a feature co-occurrence matrix (fcm) without considering trigrams
+    quanteda::fcm(tri = FALSE) %>%
+    # tidy the data using tidytext package
+    tidytext::tidy() %>%
+    # rearrange columns for better readability
+    dplyr::relocate(term, document, count) %>%
+    # rename columns for better interpretation
+    dplyr::rename(w1 = 1,
+                  w2 = 2,
+                  O11 = 3) %>%  
+    dplyr::mutate(N = sum(O11)) %>%
     
     # calculate R1, O12, and R2
     dplyr::group_by(w1) %>%
