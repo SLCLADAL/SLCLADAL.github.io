3ed58a67554377771c8bbd85f192da5aee5bf0f7
MartinSchweinberger <martin.schweinberger.hh@gmail.com>
2024-04-08 20:13:59 +1000

    update keystats

--- rscripts/keystats.R
+++ rscripts/keystats.R
@@ -14,6 +14,17 @@ keystats <- function(x){
     dplyr::mutate(Rws = nrow(.)) %>%   
     # work row-wise
     dplyr::rowwise() %>%
+    dplyr::mutate(R1 = text1+text2,
+                  R2 = N - R1,
+                  O11 = text1,
+                  O12 = R1-O11,
+                  O21 = C1-O11,
+                  O22 = C2-O12) %>%
+    dplyr::mutate(E11 = (R1 * C1) / N,
+                  E12 = (R1 * C2) / N,
+                  E21 = (R2 * C1) / N,
+                  E22 = (R2 * C2) / N) %>%
+    dplyr::select(-text1, -text2) %>%
     # calculate fishers' exact test
     dplyr::mutate(p = as.vector(unlist(fisher.test(matrix(c(O11, O12, O21, O22), 
                                                           ncol = 2, byrow = T))[1]))) %>%
