5525464be9aa5f93246ec849c8e5848b92ab1bb0
Martin Schweinberger <44954005+MartinSchweinberger@users.noreply.github.com>
2024-04-21 18:06:28 +1000

    Add files via upload

--- rscripts/loadnetdata.R
+++ rscripts/loadnetdata.R
@@ -1,3 +1,58 @@
+
+# load and format xlsx for network analysis
+
+# required packages
+require(dplyr)
+require(ggplot2)
+require(ggraph)
+require(igraph)
+require(here)
+require(openxlsx)
+require(readxl)
+require(stringr)
+require(tidyr)
+require(tidytext)
+require(tidygraph)
+
+loadnetdata <- function(x){
+  fls <- list.files(here::here(x), # path to the corpus data
+                    # full paths - not just the names of the files
+                    full.names = T) 
+  
+  # select own data (not test data)
+  fls <- fls[!stringr::str_detect(fls, "test.xlsx")]
+  
+  # loop over the vector 'myfiles' that contains paths to the data
+  dat <- readxl::read_xlsx(fls) 
+
+  n1 <- dat
+  n2 <- dat %>%
+    dplyr::rename(from = to,
+                  to = from)
+  dat <- rbind(n1, n2) 
+  
+  # Create a co-occurrence matrix
+  com <- dat %>%
+    dplyr::group_by(to, from) %>%
+    dplyr::summarise(count = n()) %>%
+    spread(from, count, fill = 0) %>%
+    as.data.frame()
+  
+  # Replace missing values with 0
+  com[is.na(com)] <- 0
+  
+  # Print the co-occurrence matrix
+  rownames(com) <- com[,1]
+  com <- com[, 2:ncol(com)]
+  
+  # create a document feature matrix
+  fcm <- quanteda::as.fcm(as.matrix(com))
+  
+  # return result
+  return(fcm)
+  
+  }
+=======
 # load and format xlsx for network analysis
 
 # required packages
@@ -50,3 +105,4 @@ loadnetdata <- function(x){
   return(fcm)
   
   }
+>>>>>>> 87f3e09316c3beb8033eaa33ed5371b0a4f40ba9
