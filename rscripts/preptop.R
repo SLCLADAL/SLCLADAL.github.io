4f6b728237403486dd2ace05347af2190db7e728
MartinSchweinberger <martin.schweinberger.hh@gmail.com>
2024-04-22 13:41:19 +1000

    topic u

---
+++ rscripts/preptop.R
@@ -0,0 +1,22 @@
+require(tidytxt)
+require(topicmodels)
+
+preptop <- function(x) {
+  x %>%
+  # convert to lower case
+  tolower() %>%
+  stringr::str_remove_all("<.*?>") %>%
+  # remove superfluous white spaces
+  stringr::str_squish() %>%
+  # remove punctuation, numbers, and symbols
+  quanteda::tokens(remove_punct = TRUE,
+                   remove_numbers = TRUE, 
+                   remove_symbol = TRUE) %>%
+  # remove stopwords and selected other words
+  quanteda::tokens_remove(pattern = c(stopwords("en"), "example", "words")) %>%
+  # convert to a document frequency matrix
+  quanteda::dfm() %>%
+    quanteda::dfm_remove(min_nchar = 3) -> clean_dfm
+  
+  return(clean_dfm)
+}
